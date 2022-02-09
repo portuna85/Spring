b191b837c5849b5e34c7358148f38f4b52bfbaf6
portuna85 <portuna85@gmail.com>
2022-02-09 11:38:29 +0900

    Spring Update

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,6 +2,7 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class HelloSpringApplication {
