04d9bc6bef06365744668b297c1d13b187b2d6cf
portuna85 <portuna85@gmail.com>
2022-02-10 22:51:05 +0900

    Spring UPDATE

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,17 +2,15 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
-
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -23,6 +21,7 @@ public class MemberService {
         memberRepository.save(member);
         return member.getId();
     }
+
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
